94e9706f668b2c59ed8bab4fd3a60691c1025b70
Mike <46776723+mkennedy87@users.noreply.github.com>
2023-01-30 23:04:03 -0700

    Update sketch.js
    vibrate buns
    random click bg color

--- HW-2/sketch.js
+++ HW-2/sketch.js
@@ -1,28 +1,56 @@
+
+var redColor = random(255);
+var greenColor = random(255);
+var blueColor = random(255);
+var alpha = random(255);
+
 function setup() {
     createCanvas(800,800);
 }
 
 function draw() {
-    background(0,100,0,50);
+var x = random(-10, 10);
+var y = random(-10, 10);
 
 
+var topBunX = 400;
+var bottomBunX = 400 + x;
+
+var pattyX = 200;
+
+var cheeseFirstX = 300;
+var cheeseTopY = 370;
+var cheeseSecondX = 500;
+var cheeseThirdX = 400;
+var cheeseThirdY = 430;
+    background(redColor,greenColor,blueColor,alpha);
+
+//text
     fill(212,175,55);
     textSize(30);
     text("Michael Kennedy", 550, 780);
     fill(255);
-    text("Das Burger", 20, 40);
-
+    text("Das Burger Buns", 20, 40);
+//top bun
     fill( 245, 222, 179 );
-    arc(400, 350, 400, 200, radians(180), radians(360), CHORD);
-
+    arc(topBunX + y, 350 + x, 400 + y, 200 + x, radians(180), radians(360), CHORD);
+//bottom bun
     fill( 245, 222, 179 );
-    arc(400, 450, 400, 200, 0, PI, CHORD);
-
+    arc(bottomBunX, 450 + x, 400 + y, 200 + x, 0, PI, CHORD);
+//meat patty
     fill( 139, 69, 19 );
-    rect(200,370,400,60);
-
+    rect(pattyX,370,400,60);
+//cheese
     fill( 255, 255, 0 );
-    triangle(300,370,500,370,400,430);
+    triangle(cheeseFirstX,cheeseTopY,cheeseSecondX,cheeseTopY,cheeseThirdX,cheeseThirdY);
 
    
+}
+
+function mouseClicked(){
+
+    redColor = random(255);
+    greenColor = random(255)
+    blueColor = random(255)
+    alpha = random(255)
 }
