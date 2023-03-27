3d5da12a88cc9794af916fe2d6d9a103297d50af
Mike <46776723+mkennedy87@users.noreply.github.com>
2023-03-27 14:39:39 -0600

    copied homework 7
    also added particle class

---
+++ Homework 8/js/animationImage.js
@@ -0,0 +1,109 @@
+class animationImage {
+
+    constructor(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.imageObjects = [];
+        this.currentAnimation;
+        this.createAnimation();
+        this.i = 0;
+        this.currentFrameCount = 0;
+        this.direction = "";
+
+
+    }
+
+    getX() {
+        return this.x;
+    }
+
+    setX(x) {
+        this.x = x;
+    }
+
+    setCurrentFrameCount(currentFrameCount) {
+
+        this.currentFrameCount = currentFrameCount;
+    }
+
+    createAnimation() {
+        this.currentAnimation = createSprite(350, 350);
+    }
+    loadAnimation(animationType, fileNames) {
+
+        this.currentAnimation.addAnimation(animationType, fileNames[0], fileNames[fileNames.length-1]);
+        this.currentAnimation.width = 200;
+        this.currentAnimation.height = 150;
+    }
+
+
+    drawAnimation(animationType) {
+        
+        this.currentAnimation.frameDelay = 2;
+        this.currentAnimation.scale = .5;
+        this.currentAnimation.changeAnimation(animationType);
+        if (animationType == 'walk' && this.direction == 'forward') {
+            this.currentAnimation.direction = 0;
+            this.currentAnimation.mirror.x = false;
+            this.currentAnimation.speed = 2;
+
+        }
+
+        else if (animationType == 'walk' && this.direction == 'up') 
+        {
+
+            this.currentAnimation.direction = 270;
+            this.currentAnimation.mirror.x = false;
+            this.currentAnimation.speed = 2;
+
+        }
+
+        else if (animationType == 'walk' && this.direction == 'reverse') 
+        {
+
+            this.currentAnimation.mirror.x = true;
+            this.currentAnimation.direction = 180;
+            this.currentAnimation.speed = 2;
+
+        }
+        
+        else if (animationType == 'walk' && this.direction == 'down') 
+        {
+
+            this.currentAnimation.direction = 90;
+            this.currentAnimation.mirror.x = false;
+            this.currentAnimation.speed = 2;
+
+        }
+        
+        else 
+        {
+
+            this.currentAnimation.velocity.x = 0;
+            this.currentAnimation.velocity.y = 0;
+
+        }
+
+
+    }
+
+
+    updatePosition(direction) 
+    {
+        
+        this.direction = direction;
+       
+    }
+
+    isColliding(myImage) {
+        return this.currentAnimation.collide(myImage);
+    }
+
+    isColliding(MyRectangle) {
+        return this.currentAnimation.collide(MyRectangle);
+    }
+    
+
+}
---
+++ Homework 8/js/sketch.js
@@ -0,0 +1,131 @@
+var i = 0;
+var myImage;
+var myAnimation;
+var myWalkAnimation;
+var idlePaths = [];
+var walkPaths = [];
+var burger;
+var burger2;
+var burger3;
+var isColliding = false;
+var rectangle1;
+var rectangleArray = [];
+var points = 0;
+var health = 0;
+
+
+function preload() {
+    idlePaths = loadStrings("./images/idle/idle.txt");
+    walkPaths = loadStrings("./images/walk/walk.txt");
+}
+
+function setup() {
+    createCanvas(800, 800);
+    myAnimation = new animationImage(0, 0, 200, 150);
+    myAnimation.loadAnimation('idle', idlePaths);
+    myAnimation.loadAnimation('walk', walkPaths);
+
+    burger = createSprite(random(30,width), random(30,height),100,100, 'static');
+    burger3 = createSprite(random(30,width), random(30,height),100,100, 'static');
+    burger2 = createSprite(random(30,width), random(30,height),100,100, 'static');
+
+  burger.img = "./images/burger3.png";
+  burger.scale = 0.1;
+  burger.diameter = 75;
+  burger2.img = "./images/burger3.png";
+  burger2.scale = 0.1;
+  burger2.diameter = 75;
+  burger3.img = "./images/burger3.png";
+  burger3.scale = 0.1;
+  burger3.diameter = 75;
+
+  rectangle1 = createSprite(random(30,width), random(30,height),100,100, 'static');
+  rectangle1 = new MyRectangle
+  burger.scale = 0.1;
+  burger.diameter = 75;
+    
+  for(var i = 0; i < 5; i++)
+  {
+      if(i % 2 == 0)
+      {
+          rectangleArray.push(new MyRectangle(random(0,width), random(0,height), random(20,100), random(10,75), 255,0,0));
+      }
+      else
+      {
+          rectangleArray.push(new MyRectangle(random(0,width), random(0,height), random(20,100), random(10,75),0,255,0));
+          
+      }
+  }
+}
+
+function draw() {
+
+    background(255,255,75);
+   
+
+
+    if (kb.pressing('d')) {
+        myAnimation.updatePosition('forward');
+        myAnimation.drawAnimation('walk');
+        if(myAnimation.isColliding(burger))
+        {
+            myAnimation.drawAnimation('idle');
+            myAnimation.updatePosition('idle');
+            
+        } 
+    }
+    else if (kb.pressing('a')) {
+        myAnimation.updatePosition('reverse');
+        myAnimation.drawAnimation('walk');
+    }
+    else if (kb.pressing('w')) {
+        myAnimation.updatePosition('up');
+        myAnimation.drawAnimation('walk');
+    }
+
+    else if (kb.pressing('s')) {
+        myAnimation.updatePosition('down');
+        myAnimation.drawAnimation('walk');
+    }
+    else {
+        myAnimation.drawAnimation('idle');
+    }
+
+  
+
+
+   /* for(var i = 0; i < rectangleArray.length; i++)
+    {
+        
+        
+        if(myAnimation.isColliding(MyRectangle))
+        {
+            if(rectangleArray[i].getG() > 0)
+            {
+                health++;
+                points++;
+            }
+            if(rectangleArray[i].getR() > 0)
+            {
+                health--;
+                points--;
+            }
+            
+            
+            rectangleArray[i] = new MyRectangle(random(0,width), random(0,height), random(20,100), random(10,75));
+        }
+    }*/
+   
+    textSize(32);
+    text("Health: " + health, 50,100);
+    text("Score: " + points, 50,50);
+    for(var i = 0; i < rectangleArray.length; i++)
+    {
+        rectangleArray[i].draw();
+    }
+   
+
+  
+    burger.debug = mouseIsPressed;
+
+}
