9d1044677cb04b24a195a4453340d7214135dea9
Mike <46776723+mkennedy87@users.noreply.github.com>
2023-04-03 15:52:05 -0600

    3 objects working

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -9,35 +9,41 @@ function setup() {
     background(175,175,175);
     
     push();
-    translate(0,0,0);
-    rotateX(frameCount * .01);
-    box(40,55,80);
+    translate(-200,-100,0);
+    rotateX(frameCount * 0.01);
+    rotateY(frameCount * 0.01);
+    box(50);
     pop();
   
     push();
-    translate(0,0,0);
-    console();
+    translate(100,-250,0);
+    rotateZ(frameCount * .03);
+    rotateX(frameCount * .015);
+    cone(50,150);
     pop();
    
-    push();
+    /*push();
     translate(0,0,0);
+    rotate(frameCount);
     plane();
     pop();
   
     push();
     translate(0,0,0);
+    rotate(frameCount);
     cylinder();
     pop();
 
     push();
     translate(0,0,0);
+    rotate(frameCount);
     torus(100, 25);
-    pop();
+    pop();*/
 
 
-push();
+    push();
     normalMaterial();
-    translate(-100,-100);
+    translate(200, 275);
     rotateZ(frameCount * 0.01);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
