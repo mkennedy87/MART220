7c2f09ab745bd484f50f10d01153afba4db4fc91
Mike <46776723+mkennedy87@users.noreply.github.com>
2023-04-03 16:20:25 -0600

    homework complete

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -1,14 +1,21 @@
-function preload() {
+let myFont;
 
+function preload() {
+  myFont = loadFont('fonts/myfont.ttf');
 }
 function setup() {
     createCanvas(800, 800, WEBGL);
   }
 
   function draw(){
-    background(175,175,175);
+    background(150,200,150);
+
+
+
+    directionalLight(150, 100, 100, -300, 300, 200);
     
     push();
+    specularMaterial(150,50,50);
     translate(-200,-150,0);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
@@ -16,6 +23,7 @@ function setup() {
     pop();
   
     push();
+    specularMaterial(0,156,0);
     translate(200,-250,0);
     rotateZ(frameCount * .03);
     rotateX(frameCount * .015);
@@ -24,6 +32,7 @@ function setup() {
    
   
     push();
+    specularMaterial(0,30,190);
     translate(50,0,0);
     rotateY(frameCount * .02);
     rotateZ(frameCount * .01);
@@ -31,6 +40,7 @@ function setup() {
     pop();
 
     push();
+    normalMaterial();
     translate(-230,240,0);
     rotateZ(frameCount * .5);
     rotateY(frameCount * .0125)
@@ -46,4 +56,10 @@ function setup() {
     rotateY(frameCount * 0.0175);
     plane(150, 150);
     pop();
+    
+    textFont(myFont);
+    textSize(36);
+    text("The Shape of Homework", -250, -300);
+    textSize(24);
+    text("By Mike Kennedy", -100, -275);
  }
