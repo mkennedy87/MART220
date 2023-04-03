d458e69c2de98890cfa80fa54fc1828fc6544865
Mike <46776723+mkennedy87@users.noreply.github.com>
2023-04-03 15:58:06 -0600

    all 5 objects added with rotations

--- Homework 9/js/sketch.js
+++ Homework 9/js/sketch.js
@@ -9,44 +9,41 @@ function setup() {
     background(175,175,175);
     
     push();
-    translate(-200,-100,0);
+    translate(-200,-150,0);
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
-    box(50);
+    box(100);
     pop();
   
     push();
-    translate(100,-250,0);
+    translate(200,-250,0);
     rotateZ(frameCount * .03);
     rotateX(frameCount * .015);
     cone(50,150);
     pop();
    
-    /*push();
-    translate(0,0,0);
-    rotate(frameCount);
-    plane();
-    pop();
   
     push();
-    translate(0,0,0);
-    rotate(frameCount);
+    translate(50,0,0);
+    rotateY(frameCount * .02);
+    rotateZ(frameCount * .01);
     cylinder();
     pop();
 
     push();
-    translate(0,0,0);
-    rotate(frameCount);
+    translate(-230,240,0);
+    rotateZ(frameCount * .5);
+    rotateY(frameCount * .0125)
     torus(100, 25);
-    pop();*/
+    pop();
 
 
     push();
     normalMaterial();
     translate(200, 275);
-    rotateZ(frameCount * 0.01);
+    rotateZ(frameCount * 0.02);
     rotateX(frameCount * 0.01);
-    rotateY(frameCount * 0.01);
+    rotateY(frameCount * 0.0175);
     plane(150, 150);
     pop();
  }
